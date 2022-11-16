db24162bfc11aae6b13d80c5e1ac51b73451f2c2
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2022-11-16 08:04:56 +0800

    update dev version
    refactor and test

--- dev_setup.py
+++ dev_setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as README:
 
 setuptools.setup(
     name="integration_testing_environment_dev",
-    version="0.0.16",
+    version="0.0.17",
     author="JE-Chen",
     author_email="zenmailman@gmail.com",
     description="Integration testing environment for web gui api load testing",
