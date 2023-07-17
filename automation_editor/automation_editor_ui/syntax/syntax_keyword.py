eff6fdbdbbc606edb74ae7a8816474f8af65c74d
JeffreyChen <zenxcvwait@gmail.com>
2023-07-17 23:05:21 +0800

    Update syntax_keyword.py

--- automation_editor/automation_editor_ui/syntax/syntax_keyword.py
+++ automation_editor/automation_editor_ui/syntax/syntax_keyword.py
@@ -1,9 +1,12 @@
 api_testka_keys: list = [
-    "test_api_method", "generate_html", "generate_html_report", "generate_json",
-    "generate_json_report", "generate_xml",
-    "generate_xml_report", "execute_action", "execute_files", "add_package_to_executor",
-    "flask_mock_server_add_router",
-    "start_flask_mock_server"]
+    "AT_test_api_method", "AT_generate_html", "AT_generate_html_report", "AT_generate_json",
+    "AT_generate_json_report", "AT_generate_xml", "AT_generate_xml_report", "AT_execute_action",
+    "AT_execute_files", "AT_add_package_to_executor", "AT_add_package_to_callback_executor",
+    "AT_flask_mock_server_add_router", "AT_start_flask_mock_server", "AT_scheduler_event_trigger",
+    "AT_remove_blocking_scheduler_job", "AT_remove_nonblocking_scheduler_job",
+    "AT_start_blocking_scheduler", "AT_start_nonblocking_scheduler",
+    "AT_start_all_scheduler", "AT_shutdown_blocking_scheduler", "AT_shutdown_nonblocking_scheduler"
+]
 
 auto_control_keys: list = [
     "AC_mouse_left", "AC_mouse_right", "AC_mouse_middle", "AC_click_mouse", "AC_get_mouse_table",
@@ -22,23 +25,31 @@ auto_control_keys: list = [
 ]
 
 web_runner_keys: list = [
-    "get_webdriver_manager", "change_index_of_webdriver", "quit", "SaveTestObject", "CleanTestObject", "set_driver",
-    "set_webdriver_options_capability", "find_element", "find_elements", "implicitly_wait", "explict_wait", "to_url",
-    "forward", "back", "refresh", "switch", "set_script_timeout", "set_page_load_timeout", "get_cookies", "get_cookie",
-    "add_cookie", "delete_cookie", "delete_all_cookies", "execute", "execute_script", "execute_async_script",
-    "move_to_element", "move_to_element_with_offset", "drag_and_drop", "drag_and_drop_offset", "perform",
-    "reset_actions",
-    "left_click", "left_click_and_hold", "right_click", "left_double_click", "release", "press_key", "release_key",
-    "move_by_offset", "pause", "send_keys", "send_keys_to_element", "scroll", "check_current_webdriver",
-    "maximize_window",
-    "fullscreen_window", "minimize_window", "set_window_size", "set_window_position", "get_window_position",
-    "get_window_rect", "set_window_rect", "get_screenshot_as_png", "get_screenshot_as_base64", "get_log", "single_quit",
-    "element_submit", "element_clear", "element_get_property", "element_get_dom_attribute", "element_get_attribute",
-    "element_is_selected", "element_is_enabled", "input_to_element", "click_element", "element_is_displayed",
-    "element_value_of_css_property", "element_screenshot", "element_change_web_element",
-    "element_check_current_web_element", "element_get_select", "set_record_enable", "generate_html",
-    "generate_html_report", "generate_json", "generate_json_report", "generate_xml", "generate_xml_report",
-    "execute_action", "execute_files", "add_package_to_executor", "add_package_to_callback_executor"]
+    "WR_get_webdriver_manager", "WR_change_index_of_webdriver", "WR_quit", "WR_SaveTestObject",
+    "WR_CleanTestObject", "WR_set_driver", "WR_set_webdriver_options_capability", "WR_find_element",
+    "WR_find_elements", "WR_implicitly_wait", "WR_explict_wait", "WR_to_url",
+    "WR_forward", "WR_back", "WR_refresh", "WR_switch", "WR_set_script_timeout", "WR_set_page_load_timeout",
+    "WR_get_cookies", "WR_get_cookie", "WR_add_cookie", "WR_delete_cookie", "WR_delete_all_cookies",
+    "WR_execute", "WR_execute_script", "WR_execute_async_script", "WR_move_to_element",
+    "WR_move_to_element_with_offset", "WR_drag_and_drop", "WR_drag_and_drop_offset", "WR_perform",
+    "WR_reset_actions", "WR_left_click", "WR_left_click_and_hold", "WR_right_click",
+    "WR_left_double_click", "WR_release", "WR_press_key", "WR_release_key",
+    "WR_move_by_offset", "WR_pause", "WR_send_keys", "WR_send_keys_to_element", "WR_scroll",
+    "WR_check_current_webdriver", "WR_maximize_window", "WR_fullscreen_window", "WR_minimize_window",
+    "WR_set_window_size", "WR_set_window_position", "WR_get_window_position",
+    "WR_get_window_rect", "WR_set_window_rect", "WR_get_screenshot_as_png", "WR_get_screenshot_as_base64",
+    "WR_get_log", "WR_single_quit", "WR_element_submit", "WR_element_clear", "WR_element_get_property",
+    "WR_element_get_dom_attribute", "WR_element_get_attribute",
+    "WR_element_is_selected", "WR_element_is_enabled", "WR_input_to_element", "WR_click_element",
+    "WR_element_is_displayed", "WR_element_value_of_css_property", "WR_element_screenshot",
+    "WR_element_change_web_element", "WR_element_check_current_web_element", "WR_element_get_select",
+    "WR_set_record_enable", "WR_generate_html", "WR_generate_html_report", "WR_generate_json",
+    "WR_generate_json_report", "WR_generate_xml", "WR_generate_xml_report",
+    "WR_execute_action", "WR_execute_files", "WR_add_package_to_executor",
+    "WR_add_package_to_callback_executor", "WR_scheduler_event_trigger", "WR_remove_blocking_scheduler_job",
+    "WR_remove_nonblocking_scheduler_job", "WR_start_blocking_scheduler", "WR_start_nonblocking_scheduler",
+    "WR_start_all_scheduler", "WR_shutdown_blocking_scheduler", "WR_shutdown_nonblocking_scheduler"
+]
 
 load_density_keys: list = [
     "start_test", "generate_html", "generate_html_report", "generate_json", "generate_json_report", "generate_xml",
