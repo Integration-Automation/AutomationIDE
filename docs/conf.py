d849b154012ee0f7377170e8a714514248f427c5
JE-Chen45 <zenjeaoaruche@gmail.com>
2023-08-11 16:53:57 +0800

    Edit doc config

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "JE-Chen"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
-extensions = ["groundwork-sphinx-theme"]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -48,7 +48,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "groundwork-sphinx-theme"
+html_theme = "groundwork"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
