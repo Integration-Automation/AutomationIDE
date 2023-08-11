aade876205c50df3decb302268336dc88f2384ec
JE-Chen45 <zenjeaoaruche@gmail.com>
2023-08-11 17:03:26 +0800

    Edit doc config

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,10 @@
 #
 import os
 import sys
+from pathlib import Path
 
 sys.path.insert(0, os.path.abspath("source"))
+sys.path.insert(0, str(Path(__file__).parent))
 
 # -- Project information -----------------------------------------------------
 
