0a6e9e62bb4c8dddf6d6c3a3537883ef0ef8d170
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2025-02-04 02:28:24 +0800

    Update stable and dev version
    Update stable and dev version

--- automation_ide/automation_editor_ui/menu/menu_utils.py
+++ automation_ide/automation_editor_ui/menu/menu_utils.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from je_editor import JEBrowser
+from je_editor import BrowserWidget
 
 if TYPE_CHECKING:
     from automation_ide.automation_editor_ui.editor_main.main_ui import AutomationEditor
@@ -11,6 +11,6 @@ if TYPE_CHECKING:
 def open_web_browser(
         automation_editor_instance: AutomationEditor, url: str, tab_name: str) -> None:
     automation_editor_instance.tab_widget.addTab(
-        JEBrowser(start_url=url),
+        BrowserWidget(start_url=url),
         f"{tab_name}{automation_editor_instance.tab_widget.count()}"
     )
